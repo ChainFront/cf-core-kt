2999ad4f2266e2c5b28e1f77ff5c29464d671e2d
Darryl Anderson <6097929+darrylanderson@users.noreply.github.com>
2019-03-08 19:42:55 -0600

    Added initial implementation of bitcoin payment API

---
+++ src/main/java/pcrypto/cf/exception/InsufficentBalanceException.java
@@ -0,0 +1,39 @@
+package pcrypto.cf.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+
+/**
+ * Exception for payment requests where the source account cannot fund the transaction.
+ */
+@ResponseStatus( HttpStatus.BAD_REQUEST )
+public class InsufficentBalanceException
+      extends AbstractApiException
+{
+    public InsufficentBalanceException( final String msg )
+    {
+        super( msg );
+    }
+
+    public InsufficentBalanceException( final String message,
+                                        final List<ErrorMessage> details )
+    {
+        super( message, details );
+    }
+
+    public InsufficentBalanceException( final String message,
+                                        final Throwable e )
+    {
+        super( message, e );
+    }
+
+    public InsufficentBalanceException( final String message,
+                                        final List<ErrorMessage> details,
+                                        final Throwable e )
+    {
+        super( message, details, e );
+    }
+}
