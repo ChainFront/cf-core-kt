ed7e30f65f1ad223a581a104cf20daeea7c53fa0
Darryl Anderson <6097929+darrylanderson@users.noreply.github.com>
2019-05-07 16:56:43 -0500

    Preparing for open source release by placing code under Apache 2.0 license.

--- src/main/java/pcrypto/cf/common/api/controller/ResourceNotFoundController.java
+++ src/main/java/pcrypto/cf/common/api/controller/ResourceNotFoundController.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2019 ChainFront LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package pcrypto.cf.common.api.controller;
 
 import org.springframework.http.HttpHeaders;
@@ -13,9 +29,9 @@ import springfox.documentation.annotations.ApiIgnore;
 
 
 /**
- * Handler for returning custom XML, JSON, or HTML responses to 404 (Not Found) situations. This needs to be a separate
- * controller (rather than part of ControllerExceptionHandler) as the 404 will be generated prior to
- * spring mvc's exception handling.
+ * Handler for returning custom XML, JSON, or HTML responses to 404 (Not Found) situations. This is a separate
+ * controller (rather than part of ControllerExceptionHandler) as the 404 is generated prior to
+ * Spring MVC exception handling.
  */
 @ApiIgnore
 @Controller
